b0e3e14c32035002f9c2e08c5c0961ffbbfbaa52
David Smith <smithdavid@microsoft.com>
2021-10-03 18:07:49 -0400

    +u rg-vwan and + rg-network

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ locals {
 }
 
 resource "azurerm_resource_group" "rg-vwan" {
-  name     = "rg-euw-core-vwan"
-  location = "West Europe"
+  name     = "rg-ussc-core-vwan"
+  location = "South Central US"
+}
+
+resource "azurerm_resource_group" "rg-network" {
+  name     = "rg-global-core-network"
+  location = "South Central US"
 }

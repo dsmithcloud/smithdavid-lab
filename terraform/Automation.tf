5173802ae2160b10e5584d2dbb5075c9789e813d
David Smith <smithdavid@microsoft.com>
2021-10-04 15:18:22 -0500

    removing terraform folder

--- terraform/Automation.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_automation_account" "autoaccount" {
-  name                = "${local.settings.naming.cloudprefix}-${local.settings.naming.locationcode}-${local.settings.naming.envlabel}-${local.settings.naming.resourcelabels.automationacct}"
-  location            = azurerm_resource_group.monitoring.location
-  resource_group_name = azurerm_resource_group.monitoring.name
-  sku_name            = local.settings.automationacct.sku_name
-  count               = local.settings.enable_automationacct ? 1 : 0
-  tags                = local.settings.tags
-}
--- terraform/RSV.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_recovery_services_vault" "rsvault" {
-  name                = "${local.settings.naming.cloudprefix}-${local.settings.naming.locationcode}-${local.settings.naming.envlabel}-${local.settings.naming.resourcelabels.rsv}"
-  location            = azurerm_resource_group.backup.location
-  resource_group_name = azurerm_resource_group.backup.name
-  sku                 = local.settings.rsv.sku
-  count               = local.settings.enable_rsv ? 1 : 0
-  tags                = local.settings.tags
-}

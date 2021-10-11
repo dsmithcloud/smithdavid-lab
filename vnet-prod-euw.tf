b81f866073a373b731bf5eda8c25ebc3b34e2f83
David Smith <smithdavid@microsoft.com>
2021-10-11 10:38:56 -0500

    fixed subnet cidr

--- vnet-prod-euw.tf
+++ vnet-prod-euw.tf
@@ -15,7 +15,7 @@ tags = "${merge(local.settings.common_tags, local.settings.prod_tags)}"
 }
 
 resource "azurerm_subnet" "subnet-euw-prod-fe" {
-    name           = "subnet-euw-prod-vnet1-fe-10.0.1.0_27"
+    name           = "subnet-euw-prod-vnet1-fe-10.1.1.0_27"
     virtual_network_name = azurerm_virtual_network.vnet-prod-euw.name
     address_prefixes = ["10.1.1.0/27"]
     resource_group_name = azurerm_resource_group.rg-network.name
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "subnet-euw-prod-fe" {
 }
 
 resource "azurerm_subnet" "subnet-euw-prod-be" {
-    name           = "subnet-euw-prod-vnet1-be-10.0.1.32_27"
+    name           = "subnet-euw-prod-vnet1-be-10.1.1.32_27"
     virtual_network_name = azurerm_virtual_network.vnet-prod-euw.name
     address_prefixes = ["10.1.1.32/27"]
     resource_group_name = azurerm_resource_group.rg-network.name

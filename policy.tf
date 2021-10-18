08abdc290c57d48807f6c9811b34e2a13fa30382
David Smith <smithdavid@microsoft.com>
2021-10-18 14:04:06 -0500

    changed parameters

--- policy.tf
+++ policy.tf
@@ -2,22 +2,20 @@ variable "policies" {
   type = map(object({
     name = string
     policy_id = string
-    parameters = string
+    parameters = any
   }))
   default = {
-      "policy1" = {
+      "1" = {
         name = "Enable Azure Monitor for VMs"
         policy_id = "/providers/Microsoft.Authorization/policySetDefinitions/55f3eceb-5573-4f18-9695-226972c6d74a"
-        parameters = <<EOL
-        {
+        parameters = {
             "logAnalytics_1": "$(azurerm_log_analytics_workspace.ussc-core-log.id)"
         }
-        EOL
       },
-      "policy2" = {
-        name = "Storage accounts should restrict network access using virtual network rules"
+      "2" = {
+        name = "Storage accounts should restrict network access"
         policy_id = "/providers/Microsoft.Authorization/policyDefinitions/2a1a9cdf-e04d-429a-8416-3bfb72a1b26f"
-        parameters = ""
+        parameters = {}
       }
   }
 }
@@ -28,6 +26,6 @@ resource "azurerm_subscription_policy_assignment" "example" {
   for_each = var.policies
   name                 = each.value.name
   policy_definition_id = each.value.policy_id
-  parameters           = each.value.parameters
+  parameters           = jsonencode(each.value.parameters)
   subscription_id      = data.azurerm_subscription.current.id
 }

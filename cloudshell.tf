85a515fec2478c4ddaa9fb23047a5bebf6ac95ab
David Smith <smithdavid@microsoft.com>
2021-11-12 14:31:52 -0600

    + subnet for storage & storage netfw rules

--- cloudshell.tf
+++ cloudshell.tf
@@ -1,7 +1,7 @@
-#================    Cloud Shell Subnet    ================
+#================    Subnets    ================
 resource "azurerm_subnet" "subnet-ussc-core-cloudshell" {
-    name           = "subnet-ussc-core-vnet1-cloudshell-10.0.0.96_28"
-    address_prefixes = ["10.0.0.96/28"]
+    name           = "subnet-ussc-core-vnet1-cloudshell-10.0.0.96_29"
+    address_prefixes = ["10.0.0.96/29"]
     resource_group_name = azurerm_resource_group.rg-network.name
     virtual_network_name = azurerm_virtual_network.vnet-core-ussc.name
     service_endpoints    = ["Microsoft.Storage"]
@@ -12,12 +12,20 @@ resource "azurerm_subnet" "subnet-ussc-core-cloudshell" {
 }
 
 resource "azurerm_subnet" "subnet-ussc-core-relay" {
-    name           = "subnet-ussc-core-vnet1-relay-10.0.0.112_28"
-    address_prefixes = ["10.0.0.112/28"]
+    name           = "subnet-ussc-core-vnet1-relay-10.0.0.104_29"
+    address_prefixes = ["10.0.0.104/29"]
     resource_group_name = azurerm_resource_group.rg-network.name
     virtual_network_name = azurerm_virtual_network.vnet-core-ussc.name
-    enforce_private_link_endpoint_network_policies = true
-    enforce_private_link_service_network_policies = false
+    enforce_private_link_endpoint_network_policies = true #true = Disable; false = Enable
+    enforce_private_link_service_network_policies = false #true = Disable; false = Enable
+}
+
+resource "azurerm_subnet" "subnet-ussc-core-storage" {
+    name           = "subnet-ussc-core-vnet1-storage-10.0.0.112_29"
+    address_prefixes = ["10.0.0.12/29"]
+    resource_group_name = azurerm_resource_group.rg-network.name
+    virtual_network_name = azurerm_virtual_network.vnet-core-ussc.name
+    service_endpoints    = ["Microsoft.Storage"]
 }
 
 #================    Network Profile    ================
@@ -94,4 +102,11 @@ resource "azurerm_private_dns_a_record" "ussc-dns-a-record" {
   resource_group_name = azurerm_resource_group.rg-network.name
   ttl                 = 3600
   records             = ["10.0.0.116"]
+}
+
+#================    Storage Firewall    ================
+resource "azurerm_storage_account_network_rules" "cshellstor-fwrules" {
+  storage_account_id = azurerm_storage_account.storussccoreadmin01.id
+  default_action             = "Allow"
+  virtual_network_subnet_ids = [azurerm_subnet.subnet-ussc-core-storage.id]
 }

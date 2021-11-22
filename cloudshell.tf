c3daf5711db2a481d015da039123bd06832a265c
David Smith <smithdavid@microsoft.com>
2021-11-22 14:06:30 -0600

    updating to use github module

--- cloudshell.tf
+++ cloudshell.tf
@@ -1,11 +1,13 @@
 module "cloudshell-vnet" {
   source                       = "git::https://github.com/dsmithcloud/tf-cloudshell-vnet.git"
-  region                       = "southcentralus"
-  existing-vnet-name           = "vnet-core-ussc-10.0.0.0_24"
-  existing-vnet-resource-group = "rg-global-core-network"
+  existing-vnet-resource-group = azurerm_resource_group.rg-network.name
+  existing-vnet-name           = azurerm_virtual_network.vnet-core-ussc.name
   ACI-OID                      = "88536fb9-d60a-4aee-8195-041425d6e927"
+  container-subnet-name        = "subnet-ussc-core-vnet1-cloudshell-10.0.0.96_27"
   container-subnet-prefix      = ["10.0.0.96/27"]
+  relay-subnet-name            = "subnet-ussc-core-vnet1-relay-10.0.0.128_26"
   relay-subnet-prefix          = ["10.0.0.128/26"]
-  storageaccount-name          = "smithdavidshell"
+  relay-namespace-name         = "subnet-ussc-core-vnet1-relay-namespace"
+  storageaccount-name          = "storusscshell01"
   tags                         = merge(local.settings.common_tags, local.settings.core_tags)
 }

1baec75820e02f687f0163d9d96a06ab25e3dba9
David Smith <smithdavid@microsoft.com>
2021-12-01 09:31:01 -0600

    added adreplication solution

--- loganalytics.tf
+++ loganalytics.tf
@@ -13,3 +13,31 @@ resource "azurerm_log_analytics_workspace" "euw-core-log" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+ resource "azurerm_log_analytics_solution" "ADReplication-ussc" {
+  solution_name         = "ADReplication"
+  location              = azurerm_resource_group.ussc-law.location
+  resource_group_name   = azurerm_resource_group.ussc-law.name
+  workspace_resource_id = azurerm_log_analytics_workspace.ussc-core-log.id
+  workspace_name        = azurerm_log_analytics_workspace.ussc-core-log.name
+  
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ADReplication"
+    
+  }
+}
+
+ resource "azurerm_log_analytics_solution" "ADReplication-euw" {
+  solution_name         = "ADReplication"
+  location              = azurerm_resource_group.euw-law.location
+  resource_group_name   = azurerm_resource_group.euw-law.name
+  workspace_resource_id = azurerm_log_analytics_workspace.euw-core-log.id
+  workspace_name        = azurerm_log_analytics_workspace.euw-core-log.name
+  
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ADReplication"
+    
+  }
+}

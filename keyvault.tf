d6f93d28cdedf99cd59f0ebfe62815aa3e353ee7
David Smith <smithdavid@microsoft.com>
2021-12-05 15:16:49 -0600

    added access policies

--- keyvault.tf
+++ keyvault.tf
@@ -14,6 +14,20 @@ resource "azurerm_key_vault" "ussc-core-kv" {
   sku_name = "standard"
 }
 
+resource "azurerm_key_vault_access_policy" "ussc-core-kv" {
+  key_vault_id = azurerm_key_vault.ussc-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
 resource "azurerm_key_vault" "euw-core-kv" {
   name                            = "euw-core-kv"
   resource_group_name             = azurerm_resource_group.euw-admin.name
@@ -26,4 +40,18 @@ resource "azurerm_key_vault" "euw-core-kv" {
   purge_protection_enabled        = true
 
   sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "euw-core-kv" {
+  key_vault_id = azurerm_key_vault.euw-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
 }

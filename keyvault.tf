5c179c7441f10f99184f72615ff1fd505772cbea
David Smith <smithdavid@microsoft.com>
2021-12-09 10:13:48 -0600

    switched to access_policy resources

--- keyvault.tf
+++ keyvault.tf
@@ -12,49 +12,54 @@ resource "azurerm_key_vault" "ussc-core-kv" {
   purge_protection_enabled        = true
 
   sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-
-    certificate_permissions = [
-      "Get",
-    ]
-  }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = "0bbb40c6-f6d6-4c9b-9760-d8c62dde5978"
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-
-    certificate_permissions = [
-      "Get",
-    ]
-  }
 }
+
+
+resource "azurerm_key_vault_access_policy" "ussc-core-kv1" {
+  key_vault_id = azurerm_key_vault.ussc-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+
+  certificate_permissions = [
+    "Get",
+  ]
+}
+
+
+resource "azurerm_key_vault_access_policy" "ussc-core-kv2" {
+  key_vault_id = azurerm_key_vault.ussc-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "0bbb40c6-f6d6-4c9b-9760-d8c62dde5978"
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+
+  certificate_permissions = [
+    "Get",
+  ]
+}
+
 
 resource "azurerm_key_vault" "euw-core-kv" {
   name                            = "euw-core-kv"
@@ -68,46 +73,49 @@ resource "azurerm_key_vault" "euw-core-kv" {
   purge_protection_enabled        = true
 
   sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-
-    certificate_permissions = [
-      "Get",
-    ]
-  }
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = "0bbb40c6-f6d6-4c9b-9760-d8c62dde5978"
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-
-    certificate_permissions = [
-      "Get",
-    ]
-  }
 }
+
+resource "azurerm_key_vault_access_policy" "euw-core-kv1" {
+  key_vault_id = azurerm_key_vault.euw-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+
+  certificate_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "euw-core-kv2" {
+  key_vault_id = azurerm_key_vault.euw-core-kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = "0bbb40c6-f6d6-4c9b-9760-d8c62dde5978"
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+
+  certificate_permissions = [
+    "Get",
+  ]
+}
+

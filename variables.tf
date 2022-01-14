cdc37752d94178bf6eeb7c4e6088bece380b5a81
David Smith <smithdavid@microsoft.com>
2022-01-13 18:12:18 -0600

    removed unused vars

--- variables.tf
+++ variables.tf
@@ -1,13 +1,1 @@
-variable "ussc-adds-ip_address" {
-  type    = list(string)
-  default = ["10.0.0.68"] # Add more IP addresses to create more domain controllers
-}
-
-variable "euw-adds-ip_address" {
-  type    = list(string)
-  default = ["10.1.0.68"] # Add more IP addresses to create more domain controllers
-}
-
-variable "admin_ospassword" {
-  type = string
-}
+variable "admin_ospassword" { type = string }
